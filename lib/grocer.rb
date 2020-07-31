28b240be87a33619eb47b5269321c9252ec4b63c
Chandler Hanson <chan.hanson5@gmail.com>
2020-07-31 01:45:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -12,8 +12,8 @@ end
 def consolidate_cart(cart)
   index = 0
   new_cart = []
-  cart.each do |grocery_item|
-    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+  cart.each do |item|
+    current_item = find_item_by_name_in_collection(item[:item], new_cart)
     if current_item
       new_cart_index = 0
       new_cart.each do |new_cart_item|
@@ -24,7 +24,7 @@ def consolidate_cart(cart)
       end
     else
       grocery_item[:count] = 1
-      new_cart << grocery_item
+      new_cart << item
     end
     index += 1
   end
